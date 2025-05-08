4a97cf895722f78114cb768b6a3460f71d3f2df8
maike jordan <maikejordan2010@hotmail.com>
2025-05-07 23:03:41 -0300

    Add Dockerfile.

--- src/pages/home/homeContext.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { ConfiguracoesTela } from "../../features/entidadesSistema/ConfiguracoesTela";
-
-
-export const telaContext = React.createContext<ConfiguracoesTela>({
-    width : 0,
-    heigth : 0
-});
